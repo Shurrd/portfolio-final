9d61ae391e7f96d0a928fbd92e7cf2c1154932ad
Shurrd <engraaadeyemi@gmail.com>
2023-10-27 16:15:26 +0100

    edited picture

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import Abraham from "../images/abraham.jpg";
+import React from 'react';
+import Abraham from '../images/abraham.jpg';
 
 const About = () => {
   return (
     <div
-      id="about"
-      className="bg-gray-800 flex flex-col md:flex-row md:justify-center justify-start md:h-[100vh] h-full items-center lg:px-24 md:px-10 px-8 md:py-0 py-12 gap-14"
+      id='about'
+      className='bg-gray-800 flex flex-col md:flex-row md:justify-center justify-start md:h-[100vh] h-full items-center lg:px-24 md:px-10 px-8 md:py-0 py-12 gap-14'
     >
-      <div className="md:block hidden h-[80%] w-[1150px] xl:w-[650px] lg:w-[900px] ">
+      <div className='md:block hidden h-[80%] w-[1150px] xl:w-[550px] lg:w-[900px]'>
         <img
           src={Abraham}
-          alt="abraham"
-          className="w-full h-full md:block rounded-sm"
+          alt='abraham'
+          className='w-full h-full md:block border rounded-full'
         />
       </div>
-      <div className="flex gap-6 flex-col">
-        <p className="text-[#a8a8e7] text-xl font-bold">WHO IS ABRAHAM ?</p>
-        <p className="text-5xl font-bold mt-[-1rem] mb-[1.5rem]">
+      <div className='flex gap-6 flex-col'>
+        <p className='text-[#a8a8e7] text-xl font-bold'>WHO IS ABRAHAM ?</p>
+        <p className='text-5xl font-bold mt-[-1rem] mb-[1.5rem]'>
           A Bit About Me
         </p>
         <p>
